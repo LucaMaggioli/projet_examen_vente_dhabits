23a07fb02b2812c4774956c605d541a445fac0f5
lulu <lucamaggio98@hotmail.com>
2022-03-31 12:51:15 +0200

    ref: cleaning code

--- ReWear_backend/ReWear_backend/Models/HealthState.cs
+++ ReWear_backend/ReWear_backend/Models/HealthState.cs
@@ -2,7 +2,7 @@
 {
     public class HealthState
     {
-        private HealthState() { } //Empty contructor is for EF that need it to make migrations, it can be private so like this noone else instead of EF can access to it
+        private HealthState() { } //Empty contructor for EF to make migrations, private so like this noone else instead of EF can access to it
         public Guid Id { get; set; }
         public string Name { get; set; }
     }
