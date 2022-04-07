fc09ae499eb32c4fb3deae1f20485ae4ece455e8
lulu <lucamaggio98@hotmail.com>
2022-04-07 13:15:40 +0200

    feat: now swashbuckle-swagger is configured with the possibility to add authorization with token

--- ReWear_backend/ReWear_backend/Program.cs
+++ ReWear_backend/ReWear_backend/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.OpenApi.Models;
 using ReWear_backend.Data;
 using ReWear_backend.Models;
 using ReWear_backend.Services;
@@ -17,7 +17,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "ReWear backend API documentation", Version = "v1.0.0" });
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+    options.AddSecurityDefinition("Bearer", securitySchema);
+    var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+
+    options.AddSecurityRequirement(securityRequirement);
+});
 
 //add Context that use Sqlite
 builder.Services.AddDbContext<ReWearDataContext>(options => options.UseSqlite(@"Data Source=ReWearTest.db;"));
