dea6fb2f8937fa37e1e19c72194409d69720598f
lulu <lucamaggio98@hotmail.com>
2022-05-12 12:49:00 +0200

    fix: get user dresses list by username

--- ReWear_backend/ReWear_backend/Controllers/UserController.cs
+++ ReWear_backend/ReWear_backend/Controllers/UserController.cs
@@ -38,7 +38,7 @@ namespace ReWear_backend.Controllers
         public async Task<IActionResult> GetDressesByUsername(string username)
         {
 
-            var user = await _userManager.FindByNameAsync(username);
+            var user = await _userManager.Users.Include(u=>u.Dresses).FirstOrDefaultAsync(u=> u.UserName == username);
 
             if (user == null) return NotFound();
 
@@ -62,14 +62,17 @@ namespace ReWear_backend.Controllers
 
         [HttpPost("me/dress")]
         [Authorize(AuthenticationSchemes = "Bearer")]
-        public async Task<IActionResult> AddDressToUser(string username, [FromBody] DressDto DressToAddDto)
+        public async Task<IActionResult> AddDressToUser([FromBody] DressDto DressToAddDto)
         {
             var userId = _httpContextAccessor.HttpContext.User.Claims.First(i => i.Type == "Id").Value;
 
             var user = await _userManager.Users.Include(u => u.Dresses).FirstOrDefaultAsync(u => u.Id == userId);
 
-
             if (user == null) return NotFound();
+            if (!user.IsPremium && user.Dresses.Count() >= 5)
+            {
+                return Unauthorized("Upgrade to Premium to have more than 5 dresses");
+            }
 
             var newDress = new Dress(DressToAddDto);
 
