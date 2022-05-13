00b939674fd1bfd78ca182ba1010f03d6b532163
kilian_aiello <kilian.aiello@gmail.com>
2022-05-13 18:42:29 +0200

    fix files

---
+++ my-app/src/Components/PageAccueil/Accueil.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export class Accueil extends Component {
+    render() {
+        return (
+            <div>
+                <body>
+
+                </body>
+            </div>
+        );
+    }
+}
