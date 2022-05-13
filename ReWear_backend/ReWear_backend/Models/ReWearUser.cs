1c3c7a563f1d3edc312d6d72e61ebd67bbd6f71f
lulu <lucamaggio98@hotmail.com>
2022-05-13 23:17:50 +0200

    add: ispremium ReWearUser Model, migration and db update; add: doc files

--- ReWear_backend/ReWear_backend/Models/ReWearUser.cs
+++ ReWear_backend/ReWear_backend/Models/ReWearUser.cs
@@ -5,6 +5,7 @@ namespace ReWear_backend.Models
     public class ReWearUser : IdentityUser
     {
         public List<Dress>? Dresses { get; set; }
+        public bool IsAdmin { get; set; }
         public bool IsPremium { get; set; }
         public DateTime EndPremiumDate { get; set; }
         //public Premium Premium { get; set; }
