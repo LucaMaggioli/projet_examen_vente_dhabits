465fb180b792c09298f57bc3404959633fdc439c
kilian_aiello <kilian.aiello@gmail.com>
2022-06-09 23:59:04 +0200

    affichage card habits

---
+++ my-app/src/Components/ImgMediaCard/ImgMediaCard.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function ImgMediaCard( props) {
+    ImgMediaCard.defaultProps = {
+        image: 'https://decizia.com/blog/wp-content/uploads/2017/06/default-placeholder.png'
+    }
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                alt=""
+                height="140"
+                image={props.image}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.description}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    {props.price}.-
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Voir plus</Button>
+            </CardActions>
+        </Card>
+    );
+}
--- my-app/src/Components/Profil/Profil.js
+++ my-app/src/Components/Profil/Profil.js
@@ -1,5 +1,6 @@
 import React, {useContext, useEffect, useState} from "react";
 import {ReWearApiContext} from "../../Services/ReWearApiContext";
+import ImgMediaCard from "../ImgMediaCard/ImgMediaCard";
 
 export default function Profil() {
     const [dresses, setDresses] = useState([]);
@@ -7,8 +8,6 @@ export default function Profil() {
         useContext(ReWearApiContext);
 
     useEffect(async () => {
-        console.log('get user dresses')
-
         let response = await request('/User/me/dresses', 'GET', null, accessToken)
         setDresses(response)
         console.log(response)
@@ -18,10 +17,12 @@ export default function Profil() {
         <>
             <h1>Profil</h1>
             <h2>Dressing</h2>
+
             {dresses.length > 0 && (
                 <ul>
                     {dresses.map(dress => (
-                        <li key={dress.id}>{dress.name}</li>
+                        <ImgMediaCard name={dress.name} description={dress.description} price={dress.price}></ImgMediaCard>
+                        //<li key={dress.id}>{dress.name}</li>
                     ))}
                 </ul>
             )}
